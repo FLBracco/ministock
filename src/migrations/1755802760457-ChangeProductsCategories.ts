468405f54bfbd2874672634255312e35a13451f1
Bracco Fabricio <braccof.0601@gmail.com>
2025-08-21 16:04:00 -0300

    feat(auth)WIP: Se agrego los onDelete Cascade en las relaciones de 'products_categories'

---
+++ src/migrations/1755802760457-ChangeProductsCategories.ts
@@ -0,0 +1,20 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class ChangeProductsCategories1755802760457 implements MigrationInterface {
+    name = 'ChangeProductsCategories1755802760457'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "products_categories" DROP CONSTRAINT "FK_f2c76a4306a82c696d620f81f08"`);
+        await queryRunner.query(`ALTER TABLE "products_categories" DROP CONSTRAINT "FK_19fe0fe8c2fcf1cbe1a80f639f1"`);
+        await queryRunner.query(`ALTER TABLE "products_categories" ADD CONSTRAINT "FK_f2c76a4306a82c696d620f81f08" FOREIGN KEY ("product_id") REFERENCES "products"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "products_categories" ADD CONSTRAINT "FK_19fe0fe8c2fcf1cbe1a80f639f1" FOREIGN KEY ("category_id") REFERENCES "categories"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "products_categories" DROP CONSTRAINT "FK_19fe0fe8c2fcf1cbe1a80f639f1"`);
+        await queryRunner.query(`ALTER TABLE "products_categories" DROP CONSTRAINT "FK_f2c76a4306a82c696d620f81f08"`);
+        await queryRunner.query(`ALTER TABLE "products_categories" ADD CONSTRAINT "FK_19fe0fe8c2fcf1cbe1a80f639f1" FOREIGN KEY ("category_id") REFERENCES "categories"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "products_categories" ADD CONSTRAINT "FK_f2c76a4306a82c696d620f81f08" FOREIGN KEY ("product_id") REFERENCES "products"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+    }
+
+}
--- src/products/entities/products_categories.entity.ts
+++ src/products/entities/products_categories.entity.ts
@@ -6,11 +6,15 @@ import { CategoryEntity } from "../../categories/entities/categories.entity";
 @Entity({name: "products_categories"})
 export class ProductCategoryEntity extends BaseEntity{
     
-    @ManyToOne(()=> ProductEntity, (products)=> products.productCategories)
+    @ManyToOne(()=> ProductEntity, (products)=> products.productCategories, {
+        onDelete: "CASCADE",
+    })
     @JoinColumn({name: "product_id"})
     product: ProductEntity;
 
-    @ManyToOne(()=> CategoryEntity, (category)=> category.productCategories)
+    @ManyToOne(()=> CategoryEntity, (category)=> category.productCategories, {
+        onDelete: "CASCADE",
+    })
     @JoinColumn({name: "category_id"})
     category: CategoryEntity;
 
