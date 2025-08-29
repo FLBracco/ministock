19389bde76c427bd949b94e775c7f645bba71cdc
Bracco Fabricio <braccof.0601@gmail.com>
2025-08-29 14:07:07 -0300

    feat(auth)WIP: Se agrego el hasheo de contraseña y la busqueda de usuario por email

--- src/user/services/user.service.ts
+++ src/user/services/user.service.ts
@@ -2,6 +2,7 @@ import { DeleteResult, UpdateResult } from "typeorm";
 import { BaseService } from "../../config/base.service";
 import { UserDTO } from "../dto/user.dto";
 import { UserEntity } from "../entities/user.entity";
+import * as bcrypt from "bcryptjs"
 
 export class UserService extends BaseService<UserEntity> {
     constructor(){
@@ -14,8 +15,18 @@ export class UserService extends BaseService<UserEntity> {
     async findUserByID(id: number): Promise<UserEntity | null> {
         return (await this.execRepository).findOneBy({ id });
     }
+    async findUserByEmail(email: string): Promise<UserEntity | null>{
+        return (await this.execRepository)
+            .createQueryBuilder("user")
+            .addSelect("user.password")
+            .where({email})
+            .getOne();
+    }
     async createUser(body: UserDTO): Promise<UserEntity> {
-        return (await this.execRepository).save(body);
+        const newUser = (await this.execRepository).create(body);
+        const hash = await bcrypt.hash(newUser.password, 10);
+        newUser.password = hash
+        return (await this.execRepository).save(newUser);
     }
     async updateUser(id: number, infoUpdate: UserDTO): Promise<UpdateResult> {
         return (await this.execRepository).update(id, infoUpdate)
